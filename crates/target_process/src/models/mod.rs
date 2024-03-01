2f2333750424e5d5111f238b72c63d2f87c98368
Federico Vitale <federico.vitale@satispay.com>
2024-03-01 15:00:50 +0100

    fix

--- crates/target_process/src/models/mod.rs
+++ crates/target_process/src/models/mod.rs
@@ -1,11 +1,28 @@
+use color_eyre::eyre::eyre;
+
 pub mod assignable;
 pub mod user;
 
+#[derive(Clone, Copy, PartialEq, Eq)]
 pub enum EntityStates {
     Open = 73,
     Planned = 74,
     InStaging = 127,
     InProgress = 75,
+}
+
+impl TryFrom<usize> for EntityStates {
+    type Error = color_eyre::eyre::Error;
+
+    fn try_from(value: usize) -> Result<Self, Self::Error> {
+        match value {
+            73 => Ok(Self::Open),
+            74 => Ok(Self::Planned),
+            75 => Ok(Self::InProgress),
+            127 => Ok(Self::InStaging),
+            _ => Err(eyre!("unable to parse entityState from usize")),
+        }
+    }
 }
 
 impl Into<usize> for EntityStates {
